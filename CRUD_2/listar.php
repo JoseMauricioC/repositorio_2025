b2e719ab5c7d34c56e5cdf44a4cc37fe412f5ba8
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-10-22 12:30:16 -0300

    para el CRUD empezamos con html, falta programar en php

---
+++ CRUD_2/listar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Personas</h1>
+        <table border='1'>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Direccion</th>
+                <th>Fecha de Nacimiento</th>
+                <th>Sexo</th>
+                <th>Telefono</th>
+                <th>Profesion</th>
+                <th>Acciones</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><a href="">Editar</a> <a href="">Eliminar</a></td>
+            </tr>
+        </table>
+
+        <a href="form_registrar.html">Registrar Nuevo</a>
+    </div>
+</body>
+</html>
